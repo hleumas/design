e59de89fedb5d21ea371f311a2f17d86a7368254
Samuel Hapak <samuel.hapak@gmail.com>
2011-07-27 01:11:05 +0200

    fixed fonts bug

--- design/app/compass/design/config.rb
+++ design/app/compass/design/config.rb
@@ -4,7 +4,7 @@ http_path = "."
 css_dir = "stylesheets"
 sass_dir = "src"
 images_dir = "images"
-fonts_dir = 'fonts'
+fonts_dir = "fonts"
 javascripts_dir = "javascripts"
 # To enable relative paths to assets via compass helper functions. Uncomment:
 #relative_assets = true
